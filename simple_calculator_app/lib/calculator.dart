2468bcfad4b86d5112178a46d591f7075804e8bd
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-08-25 21:00:04 +0100

    Added the equality functionality

--- simple_calculator_app/lib/calculator.dart
+++ simple_calculator_app/lib/calculator.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 
 class Calculator extends StatefulWidget{
@@ -14,7 +14,8 @@ class Calculator extends StatefulWidget{
 class _Calculator extends State<Calculator> {
 
   List<String> symbols = ['1','2','3','+','4','5','6','-','7','8','9','/','0','(',')','*','AC','Del', '.', '='];
-  final StreamController<String> _controller = StreamController<String>();  
+  final StreamController<List<String>> _controller = StreamController<List<String>>.broadcast(); 
+  List<String> myList = ['', ''];
 
   @override 
   void dispose(){
@@ -22,37 +23,46 @@ class _Calculator extends State<Calculator> {
     super.dispose();
   }
 
-
   bool isTapped = false;
-  String expression = '';
 
-  tap(String data){
+  tap(String a){
     setState(() => isTapped = true);
     Future.delayed(const Duration(milliseconds:10), () => setState(() => isTapped = false));
-    expression += data;
-    _controller.sink.add(expression);
+    myList[0] += a;
+    _controller.sink.add(myList);
   }
 
-  remove(data){
-    if (data.isNotEmpty){
-      final data1 = data.substring(0, data.length - 1);
-      _controller.sink.add(data1);
-      expression = data1;
+  remove(b){
+    if (b.isNotEmpty){
+      final data1 = b.substring(0, b.length - 1);
+      myList[0] = data1;      
+      _controller.sink.add(myList);
     } return;    
   }
 
-  removeAll(data){
-    if (data.isNotEmpty){
-      const data1 = '';
-      _controller.sink.add(data1);
-      expression = '';
+  removeAll(b){
+    if (b.isNotEmpty){
+      myList[0] = ''; myList[1] = '0';
+      _controller.sink.add(myList);
     } return;    
+  }
+
+  finalResult(String b){
+    try{
+      Parser p = Parser();
+    ContextModel contextModel = ContextModel();
+    Expression exp = p.parse(b);
+    double result = exp.evaluate(EvaluationType.REAL, contextModel);
+    myList[1] = result.toString();
+    _controller.sink.add(myList);
+    } catch (e) {
+      myList[1] = 'Error';
+      _controller.sink.add(myList);
+      print(e);
+    }    
   }
 
   _tap(String data) => tap(data);
-  // _remove(String data) => remove(data);
-  // _removeAll(String data) => removeAll(data);
-
 
   Widget buttons (String symbol, Color color, VoidCallback function){
     return InkWell(
@@ -80,16 +90,15 @@ class _Calculator extends State<Calculator> {
   Widget build(BuildContext context){
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
-    String result = '0'; 
 
 
-    return StreamBuilder<String>(
+    return StreamBuilder<List<String>?>(
       stream: _controller.stream,
       builder: (context, snapshot) {         
         return Scaffold(
           appBar: AppBar(
             title: const Text('Simple Calculator'), 
-            centerTitle: true, backgroundColor: Colors.deepPurpleAccent,        
+            centerTitle: true, backgroundColor: Colors.white10,        
           ),
           body: Column(
             children: [
@@ -99,8 +108,8 @@ class _Calculator extends State<Calculator> {
                   children: [
                     Positioned(
                       left: 5, top: 5,
-                      child: Text(
-                        snapshot.hasData? snapshot.data.toString(): '',
+                      child: Text(                        
+                        snapshot.hasData ? snapshot.data![0]: '',
                         style: const TextStyle(
                           color: Colors.black, fontSize: 50, fontWeight: FontWeight.bold,
                           fontFamily: 'monospace'
@@ -110,7 +119,7 @@ class _Calculator extends State<Calculator> {
                     Positioned(
                       right: 5, bottom: 5,
                       child: Text(
-                        result,
+                        snapshot.hasData? snapshot.data![1]: '',
                         style: const TextStyle(
                           color: Colors.black, fontSize: 50, fontWeight: FontWeight.bold,
                           fontFamily: 'monospace'
@@ -136,9 +145,9 @@ class _Calculator extends State<Calculator> {
                         case '.': case '(': case ')':
                           return buttons(string, Colors.blue.shade300, () => _tap(string));
                         case 'AC':  
-                          return buttons(string, Colors.red.shade700, () => removeAll(string));
-                        case 'Del': return buttons(string, Colors.red.shade300, () => remove(snapshot.data.toString()));
-                        case '=': return buttons(string, Colors.green, () => _tap(string));
+                          return buttons(string, Colors.red.shade700, () => removeAll(snapshot.data![0]));
+                        case 'Del': return buttons(string, Colors.red.shade300, () => remove(snapshot.data![0]));
+                        case '=': return buttons(string, Colors.green, () => finalResult(snapshot.data![0]));
                         default: return buttons(string, Colors.white70, () => _tap(string));
                       }
                     }                
@@ -152,4 +161,3 @@ class _Calculator extends State<Calculator> {
     );
   }
 }
-
